534be714afdc4d5f201ea0d1ca90b89aa3ab7062
Nikola Chachev <niko97213@abv.bg>
2019-03-01 12:00:21 +0200

    added file with data for the database

---
+++ databaseFiller.sql
@@ -0,0 +1,77 @@
+INSERT INTO users(username,password,user_type,profile_picture,email,city_name,country,first_name,second_name) 
+VALUES('kiko','kiki123',1,'asdprofilePicture/ManProfile.png',"kiko97@abv.bg",'sandanski','bulgaria','kiril','krastavicata');
+
+INSERT INTO users(username,password,user_type,profile_picture,email,city_name,country,first_name,second_name) 
+VALUES('bobenceto','bobi97',1,'asdprofilePicture/ManProfile.png',"borimechka@abv.bg",'sandanski','bulgaria','borislav','boreca');
+
+INSERT INTO users(username,password,user_type,profile_picture,email,city_name,country,first_name,second_name) 
+VALUES('hrissiee','hrissiee123',1,'asdprofilePicture/WomanProfile.png',"hrissiee@abv.bg",'sofia','bulgaria','hristiana','narcisa');
+
+INSERT INTO users(username,password,user_type,profile_picture,email,city_name,country,first_name,second_name) 
+VALUES('muse','theRealMuse',3,'asdprofilePicture/ManProfile.png',"officialMuse@gmail.com",'somewhere','Great Britain','','');
+
+INSERT INTO users(username,password,user_type,profile_picture,email,city_name,country,first_name,second_name) 
+VALUES('epica','alwaysEpic',3,'asdprofilePicture/ManProfile.png',"officialEpica@gmail.com",'stockholm','sweden','','');
+
+INSERT INTO users(username,password,user_type,profile_picture,email,city_name,country,first_name,second_name) 
+VALUES('lady gaga','gagaulala',3,'asdprofilePicture/crazyPic.png',"gagaga@gmail.com",'new york','USA','stefani','germanotta');
+
+INSERT INTO users(username,password,user_type,profile_picture,email,city_name,country,first_name,second_name) 
+VALUES('crazyDude99','leVski4life',2,'asdprofilePicture/ManProfile.png',"levskieVarha@abv.bg",'sofia','bulgaria','ivan','krastev');
+
+INSERT INTO users(username,password,user_type,profile_picture,email,city_name,country,first_name,second_name) 
+VALUES('cvetencetoouu','tsvetito123',2,'asdprofilePicture/WomanPic.png',"cvetenceto97@abv.bg",'sandanski','bulgaria','tsvetelina','goreplaninska');
+
+INSERT INTO users(username,password,user_type,profile_picture,email,city_name,country,first_name,second_name) 
+VALUES('chihlala','stelchencetu99',2,'asdprofilePicture/WomanPic.png',"stelaPastela99@gmail.com",'rome','italy','stela','gotinata');
+
+INSERT INTO users(username,password,user_type,profile_picture,email,city_name,country,first_name,second_name) 
+VALUES('quill18','theQuill18',2,'asdprofilePicture/ManProfile.png',"realQuill@gmail.com",'toronto','canada','chriss','lazowski');
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(12,'uprising',true,'songs/12_uprising.mp4',225);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(12,'supermasive black hole',true,'songs/12_supermasive_black_hole.mp4',160);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(13,'cry for the moon',true,'songs/13_cry_for_the_moon.mp4',312);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(13,'Feint',true,'songs/13_feint.mp4',260);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(13,'sensorium',true,'songs/13sensorium.mp4',289);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(14,'paparazzi',true,'songs/14_paparazzi.mp4',159);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(14,'telephone',true,'songs/14_telephone.mp4',210);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(14,'just dance',true,'songs/14_just_dance.mp4',185);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(13,'never neough',true,'songs/13_never_enough.mp4',287);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(15,'hail levski',true,'songs/15_hail_levski.mp4',100);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(16,'oh my',true,'songs/16_oh_my.mp4',143);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(17,'rebel',true,'songs/16_rebel.mp4',169);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(18,'be good',true,'songs/18_be_good.mp4',143);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(10,'pesent za borba',true,'songs/10_pesen_za_borba.mp4',113);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(13,'uprising',true,'songs/13_uprising.mp4',294);
+
+INSERT INTO songs(user_id,song_name,is_public,file_path,lenght) 
+VALUES(11,'sveti slance',true,'songs/11_sveti_slance.mp4',133);
